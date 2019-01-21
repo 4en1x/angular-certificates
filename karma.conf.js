4bbf01fc93c35dfcfcc2e51c449d3380086525e6
4en1x <free_playerq@mail.ru>
2019-01-21 12:26:48 +0300

    pretty logging for tests

--- karma.conf.js
+++ karma.conf.js
@@ -13,7 +13,7 @@ module.exports = function (config) {
 
     // frameworks to use
     // available frameworks: https://npmjs.org/browse/keyword/karma-adapter
-    frameworks: ['mocha'],
+    frameworks: ['jasmine'],
 
 
     // list of files / patterns to load in the browser
@@ -58,7 +58,7 @@ module.exports = function (config) {
     // test results reporter to use
     // possible values: 'dots', 'progress'
     // available reporters: https://npmjs.org/browse/keyword/karma-reporter
-    reporters: ['progress'],
+    reporters: ['spec'],
 
 
     // web server port
--- tests/editgc.test.js
+++ tests/editgc.test.js
@@ -1,29 +1,44 @@
 import { assert } from 'chai';
 import EditGCController from '../src/app/editgc/editgc.controller';
+import EditGCService from '../src/app/editgc/service/edit.service';
 
 let component;
 
 describe('EditGCController', () => {
   let $rootScope;
   let $routeParams;
+  let $location;
+  let $http;
 
   beforeEach(() => {
-    // load the module we want to test
     angular.mock.module('app');
 
-    // inject the services we want to test
-    angular.mock.inject((_$rootScope_, _$routeParams_) => {
+    angular.mock.inject((_$rootScope_, _$routeParams_, _$location_, _$http_) => {
       $rootScope = _$rootScope_;
       $routeParams = _$routeParams_;
+      $location = _$location_;
+      $http = _$http_;
     });
   });
 
 
-  it('should start with default counter value = 20', () => {
+  it('Init Edit GC When route = /add', () => {
     component = new EditGCController(
       $rootScope,
       $routeParams,
 
+    );
+    assert.deepEqual(component.scope.gc, {});
+  });
+
+  it('Init Edit GC When route = /edit/:id', () => {
+    $routeParams.id = 1;
+    component = new EditGCController(
+      $rootScope,
+      $routeParams,
+      {},
+      $location,
+      new EditGCService($http),
     );
     assert.deepEqual(component.scope.gc, {});
   });
